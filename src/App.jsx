efe14d8e3905ff997a0a6527229348eb386390c6
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-03-30 07:55:39 +0600

    Made the necessary project folder structures with tailwindcss and daisyui installed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='text-center'>   
+      Hello
+      <button className='btn btn-primary'>Hello</button>
+    </div>
+    </>
+  )
+}
+
+export default App

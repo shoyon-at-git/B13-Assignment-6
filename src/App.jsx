ae0e8c5cac7b43ad7b31cc661c57b9013f7d1377
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-03-31 00:46:24 +0600

    Added the stat section,fully responsive

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,21 @@ function App() {
       <section>
         <Banner></Banner>
       </section>
-      <section id='products' className='min-h-screen'>
-        
+      <section id='testimonial' className='bg-[#4F39F6] px-5 xl:px-50 py-4 xl:py-15 flex justify-between flex-wrap xl:flex-nowrap text-white'>
+        <div className=' border-r px-10 md:px-31 border-white/50 flex flex-col items-center gap-2 md:gap-3'>
+          <h1 className='text-3xl xl:text-6xl font-bold xl:font-extrabold'>50k+</h1>
+          <p className='font-normal xl:font-medium text-sm xl:text-2xl'>Active Users</p>
+        </div>
+        <div className=' border-white/50 flex flex-col items-center gap-2 md:gap-3'>
+          <h1 className='text-3xl xl:text-6xl font-bold xl:font-extrabold'>200+</h1>
+          <p className='font-normal xl:font-medium text-sm xl:text-2xl'>Premium Tools</p>
+        </div>
+        <div className='border-l px-10 md:px-31 flex flex-col items-center gap-2 md:gap-3'>
+          <h1 className='text-3xl xl:text-6xl font-bold xl:font-extrabold'>4.9</h1>
+          <p className='font-normal xl:font-medium text-sm xl:text-2xl'>Rating</p>
+        </div>
       </section>
+      
     </main>
     </>
   )
--- src/assets/components/navbar/navitems/NavItems.jsx
+++ src/assets/components/navbar/navitems/NavItems.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavItems = ({item}) => {
     return (
         <div>
-            <li className='font-semibold text-[#101727]'><a href={item.path}>{item.name}</a></li>
+            <li className='font-semibold text-[#101727] hover:text-blue-700'><a href={item.path}>{item.name}</a></li>
         </div>
     );
 };

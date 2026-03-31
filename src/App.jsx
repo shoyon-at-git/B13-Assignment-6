efee4122aadbdc302ba721de0d744521819c8d73
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-03-31 23:34:33 +0600

    Added testimonial section

--- src/App.jsx
+++ src/App.jsx
@@ -34,22 +34,29 @@ function App() {
                     <Banner></Banner>
                 </section>
                 <section
-                    id="testimonial"
                     className="bg-[#4F39F6] px-5 xl:px-50 py-4 xl:py-15 flex justify-between flex-wrap xl:flex-nowrap text-white"
                 >
                     <Info></Info>
                 </section>
 
-                <section className="max-w-300 mx-auto py-10 lg:py-30">
+                <section id="products" className="max-w-300 mx-auto py-10 lg:py-30">
                     <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
                         <ToolsData toolsPromise={toolsPromise} cart={cart} setCart={setCart}></ToolsData>
                     </Suspense>
                 </section>
-                <section className=" max-w-300 mx-auto py-10 lg:py-30 text-center">
+                <section id="features" className=" max-w-300 mx-auto py-10 lg:py-30 text-center">
                     <Steps></Steps>
                 </section>
-                <section className=" max-w-300 mx-auto py-10 lg:py-30">
+                <section id="pricing" className=" max-w-300 mx-auto py-10 lg:py-30">
                     <Plans planPromise={planPromise}></Plans>
+                </section>
+                <section id="testimonial" className=" py-10 lg:py-30 text-center bg-purple-600">
+                    <h1 className="font-extrabold text-2xl lg:text-4xl mb-3 text-white">Ready to Transform Your Workflow?</h1>
+                    <p className="text-gray-100/80 text-sm md:text-[16px] mb-10">Join thousands of professionals who are already using Digitools to work smarter.<br />Start your free trial today.</p>
+                    <div className="flex gap-4 justify-center">
+                        <a className="btn text-blue-700 rounded-3xl" href="#products">Explore Products</a>
+                        <a className="btn bg-transparent rounded-3xl text-white" href="#pricing">View Pricing</a>
+                    </div>
                 </section>
             </main>
         </>
